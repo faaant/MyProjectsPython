3650f35b73669c4b1efa932051d41fafe0914d6f
Illia <illiaeducation@gmail.com>
2020-10-12 13:01:13 +0300

    Upload file

---
+++ kvadrati.py
@@ -0,0 +1,19 @@
+
+s = int (input("Vvedit ploschy: "))
+while s<=0:
+    print("Vvedit korektny ploschy s.")
+    s= int(input("s = "))
+    print ("\n")
+s1= s/2
+print("\n")
+print("Ploscha menshogo kvadrata = ", str(s1))
+print("\n")
+print("Ploscha vpusanogo kvadrata mensha nizh opusanogo v 2 razu \n")
+
+
+
+
+
+
+    
+
