3193ae0389da70a558bb4f1b06258b47d0582cef
Illia <illiaeducation@gmail.com>
2020-10-20 23:02:23 +0300

    Add comments

--- kvadrati.py
+++ kvadrati.py
@@ -1,14 +1,14 @@
 
-s = int (input("Vvedit ploschy: "))
-while s<=0:
-    print("Vvedit korektny ploschy s.")
-    s= int(input("s = "))
-    print ("\n")
-s1= s/2
+s = float(input("Enter area of described square: ")) #Enter area of descrived square
+while s<=0:                                 #Check data
+    print("\nEnter the correct area s.") 
+    s= float(input("s = "))
 print("\n")
-print("Ploscha menshogo kvadrata = ", str(s1))
+s1= s/2 #search for the area of ​​the inscribed square
+n = s/s1
+print("The area of an inscribed square = ", str(s1))
 print("\n")
-print("Ploscha vpusanogo kvadrata mensha nizh opusanogo v 2 razu \n")
+print("The area of the inscribed square is smaller than area described square in "+str(n)+" times \n")
 
 
 
