a729206873a6833a75ae4dc0b2fdbb57a966ce31
Illia <illiaeducation@gmail.com>
2020-10-28 16:31:59 +0200

    Update folders

---
+++ Lab2/kolinearnist.py
@@ -0,0 +1,13 @@
+a1 = float (input())
+a2 = float (input())
+a3 = float (input())
+b1 = float (input())
+b2 = float (input())
+b3 = float (input())
+if (a1/b1==a2/b2):
+    if (a2/b2==a3/b3):
+        print("Vektoru kolinearni")
+    else:
+        print ("Vektoru ne kolinearni")
+else:
+    print("Vektoru ne kolinearni")   
